2b71ebafa24e760e829ad55a0787a1657e6e8817
Sara Nolen <nolensara1@gmail.com>
2020-11-12 20:28:04 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,8 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(value)
+  if value % 3 == 0 && value % 5 == 0
+    "Fizz"
+  elsif value % 5 == 0
+    "Buzz"
+  else value % 3 == 0
+    "Buzzfizz"
+end
